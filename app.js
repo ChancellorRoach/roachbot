d6b5d36262288cc0541619a00b936214ad9a16e7
Sieyor Roach <sieyor.roach@scotiabank.com>
2021-05-20 14:36:43 -0400

    update twitch/discord

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var cors = require('cors');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -10,6 +11,7 @@ var usersRouter = require('./routes/users');
 var app = express();
 
 // view engine setup
+app.set(cors())
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
@@ -18,6 +20,25 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(function (req, res, next) {
+
+  // Website you wish to allow to connect
+  res.setHeader('Access-Control-Allow-Origin', '*');
+
+  // Request methods you wish to allow
+  res.setHeader('Access-Control-Allow-Methods', 'GET');
+
+  // Request headers you wish to allow
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+  // Set to true if you need the website to include cookies in the requests sent
+  // to the API (e.g. in case you use sessions)
+  res.setHeader('Access-Control-Allow-Credentials', true);
+
+  // Pass to next layer of middleware
+  next();
+});
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
@@ -37,5 +58,6 @@ app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error');
 });
+
 
 module.exports = app;
--- routes/users.js
+++ routes/users.js
@@ -1,25 +1,81 @@
-var express = require('express');
-var router = express.Router();
-var constants = require('../util/constants');
-const Discord = require('discord.js');
-intents = Discord.Intents.ALL
-const client = new Discord.Client(intents);
+var express         = require('express');
+var passport        = require('passport');
+var OAuth2Strategy  = require('passport-oauth').OAuth2Strategy;
+var router          = express.Router();
+var constants       = require('../util/constants');
+
+const Discord       = require('discord.js');
+intents             = Discord.Intents.ALL
+const client        = new Discord.Client(intents);
+
+client.login(constants.CLIENT_TOKEN);
+
+client.on('ready', () => {
+    console.log("ready")
+
+});
+
+client.on('message', msg => {
+    if (msg.content.includes('hello')) {
+        msg.reply('it\'s a trap');
+    }
+});
+
+// Override passport profile function to get user profile from Twitch API
+OAuth2Strategy.prototype.userProfile = function(accessToken, done) {
+    var options = {
+        url: 'https://api.twitch.tv/helix/users',
+        method: 'GET',
+        headers: {
+            'Client-ID': constants.TWITCH_CLIENT_ID,
+            'Accept': 'application/vnd.twitchtv.v5+json',
+            'Authorization': 'Bearer ' + accessToken
+        }
+    };
+
+    request(options, function (error, response, body) {
+        if (response && response.statusCode == 200) {
+            done(null, JSON.parse(body));
+        } else {
+            done(JSON.parse(body));
+        }
+    });
+}
+
+passport.serializeUser(function(user, done) {
+    done(null, user);
+});
+
+passport.deserializeUser(function(user, done) {
+    done(null, user);
+});
+
+passport.use('twitch', new OAuth2Strategy({
+        authorizationURL: 'https://id.twitch.tv/oauth2/authorize',
+        tokenURL: 'https://id.twitch.tv/oauth2/token',
+        clientID: constants.TWITCH_CLIENT_ID,
+        clientSecret: constants.TWITCH_SECRET,
+        state: true
+    },
+    function(accessToken, refreshToken, profile, done) {
+        profile.accessToken = accessToken;
+        profile.refreshToken = refreshToken;
+
+        // Securely store user profile in your DB
+        //User.findOrCreate(..., function(err, user) {
+        //  done(err, user);
+        //});
+
+        done(null, profile);
+    }
+));
+
+// Set route to start OAuth link, this is where you define scopes to request
+router.get('/auth', passport.authenticate('twitch', { scope: 'user_read' }));
+
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-
-    client.on('ready', () => {
-        console.log("ready")
-
-    });
-
-    client.on('message', msg => {
-        if (msg.content.includes('hello')) {
-            msg.reply('it\'s a trap');
-        }
-    });
-
-    client.login(constants.CLIENT_TOKEN);
 
     const statusArray = [];
 
@@ -34,15 +90,16 @@ router.get('/', function(req, res, next) {
 
         for (let index = 0; index < activities.length; index++){
             console.log("activity: %j", activities[index])
-            let details = ""
+
+            let action = activities[index].state
             if(activities[index].details != null){
-                details = " - " + activities[index].details
+                action = action + " - " + activities[index].details
             }
 
             let status = {
                 type: activities[index].type,
                 name: activities[index].name,
-                action: activities[index].state + details
+                action: action
             }
 
             statusArray.push(status)
